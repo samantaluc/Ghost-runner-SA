69f30b11279a1e49ddbcc6aae899cc0e9dafc25e
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-01-03 10:56:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
-  
 var towerImg, tower;
 var doorImg, door, doorsGroup;
 var climberImg, climber, climbersGroup;
 var ghost, ghostImg;
 var invisibleBlockGroup, invisibleBlock;
 var gameState = "play"
-
-
 function preload(){
   towerImg = loadImage("tower.png");
   doorImg = loadImage("door.png");
@@ -14,61 +11,38 @@ function preload(){
   ghostImg = loadImage("ghost-standing.png");
   spookySound = loadSound("spooky.wav");
 }
-
 function setup() {
   createCanvas(600,600);
   spookySound.loop();
   tower = createSprite(300,300);
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
-  
   doorsGroup = new Group();
   climbersGroup = new Group();
   invisibleBlockGroup = new Group();
-  
   ghost = createSprite(200,200,50,50);
   ghost.scale = 0.3;
   ghost.addImage("ghost", ghostImg);
 }
-
-
 function draw() {
   background(255);
- 
-  
-  if (gameState === "play") {
-    
-    if(keyDown("left_arrow")){
+   if (gameState === "play") {
+      if(keyDown("left_arrow")){
         ghost.x = ghost.x - 3;
-
       // escreva o código para mover para a esquerda quando a tecla para a esquerda for pressionada
-    }
-    if(keyDown("right_arrow")){
-  
-          ghost.x = ghost.x + 3;
-
-      // escreva o código para mover para a direita quando a tecla para a direita for pressionada
-      
-    }
-    if(keyDown("space")){
-  
-         ghost.velocityY = -10;
-
-      // escreva o código para mover para cima quando a tecla espaço for pressionada
-      
-    }
-  
-  ghost.velocityY = ghost.velocityY + 0.8;
-  
-   
-      //escreva uma condição para a torre de rolagem infinita
-    if(tower.y > 400){ // adicionado o valor de 400 para a altura y
-      tower.y = 300
-    } 
-    
-      spawnDoors();
-
-  
+        }
+       if(keyDown("right_arrow")){
+            ghost.x = ghost.x + 3; // escreva o código para mover para a direita quando a tecla para a direita for pressionada
+        }
+       if(keyDown("space")){
+          ghost.velocityY = -10;// escreva o código para mover para cima quando a tecla espaço for pressionado
+        }
+      ghost.velocityY = ghost.velocityY + 0.8;      
+    //escreva uma condição para a torre de rolagem infinita
+      if(tower.y > 400){ // adicionado o valor de 400 para a altura y
+         tower.y = 300
+       }       
+    spawnDoors(); 
 //escreva um código para fazer invisibleBlockGroup (grupo de bloco invisível) colidir com o fantasma, destruir o fantasma e mudar o estado do jogo para end.
      if(climbersGroup.isTouching(ghost)){
       ghost.velocityY = 0;
@@ -77,21 +51,18 @@ function draw() {
       ghost.destroy();
       gameState = "end"
     }
-    
-  
   drawSprites();
 }
   if (gameState === "end"){
     stroke("yellow");
     fill("yellow");
     textSize(30);
-    text("Game Over", 230,250)
+    text("Game Over", 230,250);
   }
 }
-
 function spawnDoors()
  {
-  //escreva o código aqui para gerar as nuvens
+  //escreva o código aqui para gerar as portas
   if (frameCount % 240 === 0) {
     var door = createSprite(200, -50);
     var climber = createSprite(200,10);
@@ -102,7 +73,6 @@ function spawnDoors()
     door.x = Math.round(random(120,400));
     door.addImage(doorImg);
     climber.addImage(climberImg);
-    
     door.velocityY = 1;
     climber.velocityY = 1;
     invisibleBlock.velocityY = 1;
